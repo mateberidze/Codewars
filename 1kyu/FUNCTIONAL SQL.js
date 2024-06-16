20c8ab0563bc814b8330c151d7a5a109925a2cdf
Vovaadidas777 <mateberidze464@gmail.com>
2024-06-16 23:16:25 +0400

    uptade commit

--- 1kyu/FUNCTIONAL SQL.js
+++ 1kyu/FUNCTIONAL SQL.js
@@ -1,6 +1,5 @@
 const { Query, QueryData } = require('./query');
-const { BoolFunc } = require('./types');
-
+const { BoolFunc } = require('./types')
 function query() {
   const sqlQuery = new Query();
   return {
@@ -18,3 +17,30 @@ console.log(
     .select(d => d * 2)
     .execute()
 );
+
+
+// for (let i = 0; i <= 10; i++) {  
+//   if (i % 2 === 0) {
+//       console.log(`{i} is even`);
+//   } else {
+//       console.log(`{i} is odd`);
+//   }
+// }
+
+
+
+// let password = "mate007"; 
+// let attempts = 0;
+// let maxAttempts = 3;
+
+// while (attempts < maxAttempts) {
+//   let userInput = prompt("Enter your password:");
+//   if (userInput === password) {
+//     console.log("You have access.");
+//   } else {
+//     console.log("Incorrect. Try again!");
+//     if (attempts === maxAttempts) {
+//       console.log("Maximum attempts is end.");
+//     }
+//   }
+// }
--- 3kyu/battleship battle validator.js
+++ 3kyu/battleship battle validator.js
@@ -10,3 +10,4 @@ function validateBattlefield(field) {
     } } }
     return [0,4,3,2,1].every((s,i) => s == ships[i]);
   }
+
